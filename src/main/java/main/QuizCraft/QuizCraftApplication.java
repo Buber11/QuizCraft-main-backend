5c6ad219f79c3ddd53399d61dcb7c109ace21d55
Buber11 <105680119+Buber11@users.noreply.github.com>
2024-11-23 19:16:39 +0100

    init

---
+++ src/main/java/main/QuizCraft/QuizCraftApplication.java
@@ -0,0 +1,13 @@
+package main.QuizCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizCraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/main/QuizCraft/QuizCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package main.QuizCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
