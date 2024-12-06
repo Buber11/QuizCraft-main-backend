d8fa0340c83482d8fb6e3c9f2fea19b9a1917a20
Buber11 <105680119+Buber11@users.noreply.github.com>
2024-12-06 11:50:47 +0100

    feature: add dashboard

--- Front-end/QuizCraft/src/app/app.routes.ts
+++ Front-end/QuizCraft/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
